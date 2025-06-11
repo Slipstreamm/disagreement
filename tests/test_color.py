669f00e745eb85f70e90166bd84a2a69995b20f7
Slipstream <me@slipstreamm.dev>
2025-06-10 18:42:52 -0600

    Add extensive color helper methods (#40)

--- tests/test_color.py
+++ tests/test_color.py
@@ -11,3 +11,6 @@ def test_static_colors():
     assert Color.red().value == 0xFF0000
     assert Color.green().value == 0x00FF00
     assert Color.blue().value == 0x0000FF
+    assert Color.blurple().value == 0x5865F2
+    assert Color.light_blurple().value == 0xE0E3FF
+    assert Color.legacy_blurple().value == 0x7289DA
