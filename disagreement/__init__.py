e39f701f33674515707da6cc585559dd0edd2658
Slipstream <faxcluster20@protonmail.com>
2025-06-11 17:30:34 -0600

    bump version to 0.4.0 in __init__.py and pyproject.toml

--- disagreement/__init__.py
+++ disagreement/__init__.py
@@ -12,7 +12,7 @@ __title__ = "disagreement"
 __author__ = "Slipstream"
 __license__ = "BSD 3-Clause License"
 __copyright__ = "Copyright 2025 Slipstream"
-__version__ = "0.3.0b1"
+__version__ = "0.4.0"
 
 from .client import Client, AutoShardedClient
 from .models import Message, User, Reaction, AuditLogEntry
